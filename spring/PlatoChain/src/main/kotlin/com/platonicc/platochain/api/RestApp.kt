98e6a35c31054e8154e887fd9da7078579cdec1d
Lucifer † <35570939+amannirala13@users.noreply.github.com>
2020-07-14 02:45:49 +0530

    Added API endpoints for reading chain and mining over the blockchain

--- spring/PlatoChain/src/main/kotlin/com/platonicc/platochain/api/RestApp.kt
+++ spring/PlatoChain/src/main/kotlin/com/platonicc/platochain/api/RestApp.kt
@@ -2,26 +2,29 @@ package com.platonicc.platochain.api
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.google.gson.JsonArray
-import com.platonicc.platochain.blockchain.Block
+import com.platonicc.platochain.blockchain.BlockData
+import com.platonicc.platochain.blockchain.Chain
 import com.platonicc.platochain.blockchain.Chain.Companion.localChain
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.view.RedirectView
 
 class RestApp {
     @RestController
     object GET{
 
-        @GetMapping(value =[ "/chain", "/Chain", "/getChain", "/getchain"])
+        @GetMapping(value =[ "/chain", "/Chain", "/getChain", "/GetChain","/getchain"])
         fun sendChain(): String{
             return ObjectMapper().writeValueAsString(localChain)
         }
-        
     }
 
+    @RestController
     object POST{
-
+        @PostMapping(value=["/mine","/Mine", "/minBlock", "/MineBlock", "mineblock"])
+        fun minBlock(@RequestBody dataString:String):RedirectView{
+            Chain.addBlock(ObjectMapper().readValue(dataString, object: TypeReference<BlockData>(){}))
+            return RedirectView("/chain")
+        }
     }
 
 }
